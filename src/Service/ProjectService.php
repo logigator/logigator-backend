4eec7aa3222b23fdbbfd89cf6def081edbb71de2
Stefan Mikulik <stefanmikulik0604@gmail.com>
2019-08-31 14:34:21 +0200

    added uuid library

--- src/Service/ProjectService.php
+++ src/Service/ProjectService.php
@@ -8,17 +8,41 @@
 
 namespace Logigator\Service;
 
+use Ramsey\Uuid\Exception\UnsatisfiedDependencyException;
+use Ramsey\Uuid\Uuid;
+
 class ProjectService extends BaseService
 {
+	private const DEFAULT_IMAGE_LOCATION = "";
 
-	public function createProject($name, $isComponent){
+	// TODO: set a default image location
+
+	public function createProject($name, $isComponent, $fk_user)
+	{
+		$location = $this->generateLocation($name);
 		$this->container->get('DbalService')
 			->insert('projects')
 			->setValue('name', '?')
 			->setValue('isComponent', '?')
+			->setValue('fk_user', '?')
+			->setValue('location', '?')
+			->setValue('preview_image', '?')
 			->setParameter(0, $name)
 			->setParameter(1, $isComponent)
+			->setParameter(2, $fk_user)
+			->setParameter(3, $location)
+			->setParameter(4, self::DEFAULT_IMAGE_LOCATION);
 		;
+	}
+
+	private function generateLocation($name){
+		try {
+			$uuid1 = Uuid::uuid1();
+			return $name.$uuid1->toString();
+		} catch (UnsatisfiedDependencyException $e) {
+			echo 'Caught exception: ' . $e->getMessage() . "\n";
+		}
+		return "errorFile";
 	}
 
 }
