7a2015a4e7fd6e6103d3a308e1ffa5d2d280fe30
Andreas Schlößl <andreas.sch4@gmail.com>
2019-09-04 11:19:41 +0200

    Update GetAllProjectsInfo.php

--- src/Api/Projects/GetAllProjectsInfo.php
+++ src/Api/Projects/GetAllProjectsInfo.php
@@ -18,13 +18,7 @@ class GetAllProjectsInfo extends BaseController
 {
 	public function __invoke(ServerRequestInterface $request, Response $response, array $args)
 	{
-
-		if (!$this->isUserAuthenticated()) {
-			return ApiHelper::createJsonResponse($response, null, 401, 'Not logged in');
-		}
-
 		$data = $this->container->get('ProjectService')->getAllProjectsInfo($this->getTokenPayload()->sub);
 		return ApiHelper::createJsonResponse($response, $data);
-
 	}
 }
