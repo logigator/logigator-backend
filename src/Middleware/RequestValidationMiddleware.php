b9dd6c4f608664e0b59453ceb7ef10fd8aff37e5
Andreas Schloessl <andreas.sch4@gmail.com>
2019-09-24 21:04:34 +0200

    Request Validation now also for GET Parameters Every route is broken, do not merge. Fixed bug where multi-level object in POST would get recognized as array ParsedBody now Object instead of associative array

--- src/Middleware/RequestValidationMiddleware.php
+++ src/Middleware/RequestValidationMiddleware.php
@@ -20,25 +20,30 @@ class RequestValidationMiddleware
 	}
 
 	public function __invoke(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface {
-		if ($request->getMethod() === 'GET') {
-			return $handler->handle($request);
-		}
-
-		if (strpos($request->getHeaderLine('Content-Type'), 'application/json') !== false) {
-            $request = $request->withParsedBody(json_decode($request->getBody()->getContents(), true));
-			if($request->getParsedBody() === null)
-				throw new HttpBadRequestException($request, 'Invalid JSON received.');
-        }
-
-		$route = RouteContext::fromRequest($request)->getRoute()->getPattern();
+		$route = RouteContext::fromRequest($request)->getRoute();
 		if(!$route)
 			throw new \Exception();
 
-		$path = ApiHelper::getPath($this->container->get('ConfigService')->getConfig('json_schemas'), trim($route, ' /') . '.json');
+		if ($request->getMethod() !== 'GET')  {
+			if(strpos($request->getHeaderLine('Content-Type'), 'application/json') !== false) {
+				$request = $request->withParsedBody(json_decode($request->getBody()->getContents()));
+				if ($request->getParsedBody() === null)
+					throw new HttpBadRequestException($request, 'Invalid JSON received.');
+			} else {
+				$request = $request->withParsedBody(json_decode(json_encode($request->getParsedBody())));
+				if ($request->getParsedBody() === null)
+					throw new HttpBadRequestException($request, 'Request was malformed.');
+			}
+        }
+
+		$pattern = trim(explode('[', explode('{', $route->getPattern())[0])[0], ' /');
+
+		$path = ApiHelper::getPath($this->container->get('ConfigService')->getConfig('json_schemas'), $pattern . '.json');
 		if(!file_exists($path))
 			throw new HttpBadRequestException($request, 'Invalid data received: Failed to validate input');
 
-		$parsedBody = (object)$request->getParsedBody();
+		$parsedBody = (object)[ 'arguments' => (object)$route->getArguments(), 'body' => $request->getParsedBody() ];
+
 		$validator = new \JsonSchema\Validator();
 		$validator->validate($parsedBody, [ '$ref' => $path ]);
 		if($validator->isValid())
