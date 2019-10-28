55ce7fb90c68df5170c1215b79f85f17ae44aefc
Andreas <andreas.sch4@gmail.com>
2019-10-28 18:13:41 +0100

    implement recaptcha

--- src/Api/Auth/RegisterEmail.php
+++ src/Api/Auth/RegisterEmail.php
@@ -8,6 +8,7 @@ use Logigator\Api\BaseController;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Message\ResponseInterface;
 use Slim\Exception\HttpBadRequestException;
+use Slim\Exception\HttpForbiddenException;
 
 class RegisterEmail extends BaseController
 {
@@ -23,7 +24,23 @@ class RegisterEmail extends BaseController
             throw new HttpBadRequestException($request, 'Username has already been taken.');
         }
 
-		//TODO: Recaptcha
+        $recaptcha = json_decode(file_get_contents('https://www.google.com/recaptcha/api/siteverify', false, stream_context_create([
+        	'http' => [
+        		'header' => 'Content-Type: application/x-www-form-urlencoded\r\n',
+		        'method' => 'POST',
+		        'content' => http_build_query([
+		        	'secret' => $this->container->get('ConfigService')->getConfig('google_recaptcha_secret'),
+			        'response' => $body->recaptcha,
+			        'remoteip' => $_SERVER['REMOTE_ADDR']
+		        ])
+	        ]
+        ])));
+
+        if (!$recaptcha || !isset($recaptcha->success))
+        	throw new \Exception('Could not verify ReCaptcha.');
+
+        if($recaptcha->success !== true)
+        	throw new HttpForbiddenException($request, 'ReCaptcha is invalid.');
 
 		$this->container->get('UserService')->createUser($body->username, null, $body->email, 'local', $body->password);
 		$this->container->get('AuthenticationService')->setUserAuthenticated($this->container->get('UserService')->fetchUserIdPerEmail($body->email), 'local');
